ac74e6fb10390221d7a2c3691f8c82f87a4d24cd
阿部竜弥 <abetatsuya@ABEMacBookPro.local>
2025-05-01 01:10:27 +0900

    Save local changes to user_config.py

--- agents/user_config.py
+++ agents/user_config.py
@@ -3,5 +3,5 @@ from agents.random_agent import RandomAgent
 from agents.vlm_agent import LlamaVisionModel
 from agents.first_agent import FirstAgent
 
-UserAgent = FirstAgent
-#UserAgent = SimpleRAGAgent
+#UserAgent = FirstAgent
+UserAgent = SimpleRAGAgent
